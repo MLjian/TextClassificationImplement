ce1c81181e29b0f254a866e421c762f38a97b090
Jian <1069525306@qq.com>
2018-08-27 21:05:43 +0800

    修改lgb参数

--- ml/code/config.py
+++ ml/code/config.py
@@ -29,7 +29,7 @@ clfs = {
                         nthread=1, gamma=0, min_child_weight=1, max_delta_step=0, subsample=1, colsample_bytree=1,
                         colsample_bylevel=1, reg_alpha=0, reg_lambda=1, scale_pos_weight=1, base_score=0.5, seed=0,
                         missing=None),
-    'lgb': lgb.LGBMClassifier(boosting_type='gbdt', num_leaves=31, max_depth=6, learning_rate=0.1, n_estimators=100,
+    'lgb': lgb.LGBMClassifier(boosting_type='gbdt', num_leaves=31, max_depth=-1, learning_rate=0.1, n_estimators=100,
                               max_bin=255, subsample_for_bin=200000, objective=None, min_split_gain=0.0, min_child_weight=0.001,
                               min_child_samples=20, subsample=1.0, subsample_freq=1, colsample_bytree=1.0, reg_alpha=0.0,
                               reg_lambda=0.0, random_state=None, n_jobs=-1, silent=True)
